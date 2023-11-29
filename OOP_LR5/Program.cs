0c43be989b4b4e56852e8b6e971f3d827266cd8b
skr3 <skr3@lab4.com>
2023-11-29 12:27:30 +0200

    Initial commit - project structure

---
+++ OOP_LR5/Program.cs
@@ -0,0 +1,11 @@
+﻿using OOP_LR5;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        
+    }
+}
